3a1a39b84c9b944940dbf7d63a9caafa8a3b7144
telamonas9 <mariosdelivo_9@hotmail.com>
2015-11-24 16:14:39 +0200

    Update Checker.java

--- Checker.java
+++ Checker.java
@@ -2,10 +2,12 @@
 public class Checker {
 	int pos;
 	boolean color;
+	Checker nextCh;
 	
 	public Checker(int pos, boolean color){
 		this.pos = pos;
 		this.color = color;
+		nextCh = null;
 	}
 	public boolean isBlack(){
 		return color;
