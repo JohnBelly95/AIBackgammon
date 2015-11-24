1ba9f0703c08f4691c6b2733de19767971f47e0f
telamonas9 <mariosdelivo_9@hotmail.com>
2015-11-24 10:18:22 +0200

    Update MiniMaxTree.java

--- MiniMaxTree.java
+++ MiniMaxTree.java
@@ -2,9 +2,9 @@ import java.util.ArrayList;
 import java.util.ArrayList;
 
 public class MinimaxTree {
-	private int[] move;
+	private Board move;
 	private State root;
-	private ArrayList<State> children;
+	private ArrayList<Board> children;
 	
 	public MinimaxTree(State r){
 		root = r;
@@ -20,5 +20,22 @@ public class MinimaxTree {
 	
 	
 	
+	
+}
+
+
+
+public Board expectiMinimax(Board board,int d1,int d2){
+	children = new ArrayList<Board>();
+	Board Max;
+	while(true){
+		if(getNextMove == null){// H MEXRI NA TELEIWSEI H NEXTMOVE
+			break;
+		}
+
+		Board child = new Board(board.getNextMove(d1,d2));
+		children.add(child);
+		
+	}
 	
 }
