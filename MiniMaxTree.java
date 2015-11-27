539680feb550e881b796a6489937d42ecc6323ce
telamonas9 <mariosdelivo_9@hotmail.com>
2015-11-27 17:54:35 +0200

    Update MiniMaxTree.java

--- MiniMaxTree.java
+++ MiniMaxTree.java
@@ -41,20 +41,24 @@ public Board Max(Board board,int d1,int d2,int depth,int alpha, int beta){
 	}
 	
 	Board maxMove = new Board(Integer.MIN_VALUE);
-	while(true){
-		Board maxChild =new Board(getNextMove(board,d1,d2));
-		if(maxChild == null) break;
-		for(int i=0; i<21; i++){
-
-			Board minChild = new Board(Min(maxChild,luck[i][1],luck[i][2],depth + 1, alpha, beta));
-			if(minChild.getValue() > maxMove.getValue() ){  // TRUE EINAI TO MAVRO
-				maxMove.setBoard(minChild.getBoard());
-				maxMove.setValue(minChild.getValue());
+		
+			Board maxChild =new Board(getNextMove(board,d1,d2));
+			if(maxChild == null) break;
+			for(int i=0; i<21; i++){
+	
+				Board minChild = new Board(Min(maxChild,luck[i][1],luck[i][2],depth + 1, alpha, beta));
+				if(minChild.getValue() > maxMove.getValue() ){  // TRUE EINAI TO MAVRO
+					maxMove.setBoard(minChild.getBoard());
+					maxMove.setValue(minChild.getValue());
+				}
+				if(minChild.getValue() >= beta) return maxMove;
+				if(minChild.getValue() > alpha) alpha = minChild.getValue();
 			}
-			if(minChild.getValue() >= beta) return maxMove;
-			if(minChild.getValue() > alpha) alpha = minChild.getValue();
-		}
-	}
+
+		
+		
+		
+		
 	
 	return maxMove;
 	
@@ -68,7 +72,9 @@ public Board Min(Board board,int d1,int d2,int depth,int alpha, int beta){
 	
 	Board minMove = new Board(Integer.MAX_VALUE);
 	while(true){
-		Board minChild =new Board(getNextMove(board,d1,d2));
+		
+		Board minChild =new Board(board);
+		
 		if(minChild == null) break;
 		for(int i=0; i<21; i++){
 
@@ -80,11 +86,14 @@ public Board Min(Board board,int d1,int d2,int depth,int alpha, int beta){
 			if(maxChild.getValue() <= alpha) return minMove;
 			if(maxChild.getValue() < beta) beta = minChild.getValue();
 		}
+		
+		
 	}
 	
 	return minMove;
 	
 }
+
 
 
 
@@ -121,3 +130,127 @@ private int[][] filluck(){         // GEMIZW THN TYXH
 	}
 	return l;
 }
+
+
+
+
+if(d1 == d2){//DIPLES
+	for(int move1=0; move1<24; move1++){
+		Board maxChild =new Board(board);//to maxChild ginetai to tamplo 
+		maxChild.setValue(maxChild.evaluate(true));
+		
+		if(maxChild.noMoreMoves(d1)){
+			
+			//MAX 
+			for(int i=0; i<21; i++){
+				
+				Board minChild = new Board(Min(maxChild,luck[i][1],luck[i][2],depth + 1, alpha, beta));
+				if(minChild.getValue() > maxMove.getValue() ){  // TRUE EINAI TO MAVRO
+					maxMove.setBoard(minChild.getBoard());
+					maxMove.setValue(minChild.getValue());
+				}
+				if(minChild.getValue() >= beta) return maxMove;
+				if(minChild.getValue() > alpha) alpha = minChild.getValue();
+			}//KLEINEI MAX
+			break;
+		}
+		else if( (move1 + d1 < 24) && board.canMoveCh(move1, move1 + d1)){
+			maxChild.moveCh(move1, move1 + d1);
+			maxChild.setValue(maxChild.evaluate(true));
+			
+			for(int move2=0; move2<24; move2++){//DEUTERH KINHSH 
+				if(board.noMoreMoves(d1)){
+					//MAX
+					for(int i=0; i<21; i++){
+						
+						Board minChild = new Board(Min(maxChild,luck[i][1],luck[i][2],depth + 1, alpha, beta));
+						if(minChild.getValue() > maxMove.getValue() ){  // TRUE EINAI TO MAVRO
+							maxMove.setBoard(minChild.getBoard());
+							maxMove.setValue(minChild.getValue());
+						}
+						if(minChild.getValue() >= beta) return maxMove;
+						if(minChild.getValue() > alpha) alpha = minChild.getValue();
+					}//KLEINEI MAX
+					break;
+				}
+				else if( (move2 + d1 < 24) && board.canMoveCh(move2, move2 + d1)){
+					maxChild.moveCh(move2, move2 + d1);
+					maxChild.setValue(maxChild.evaluate(true));
+				
+					for(int move3=0; move3<24; move3++){//TRITH KINHSH 
+						if(board.noMoreMoves(d1)){
+							//MAX
+							for(int i=0; i<21; i++){
+								
+								Board minChild = new Board(Min(maxChild,luck[i][1],luck[i][2],depth + 1, alpha, beta));
+								if(minChild.getValue() > maxMove.getValue() ){  // TRUE EINAI TO MAVRO
+									maxMove.setBoard(minChild.getBoard());
+									maxMove.setValue(minChild.getValue());
+								}
+								if(minChild.getValue() >= beta) return maxMove;
+								if(minChild.getValue() > alpha) alpha = minChild.getValue();
+							}//KLEINEI MAX
+							break;
+						}
+						else if( (move3 + d1 < 24) && board.canMoveCh(move3, move3 + d1)){
+							maxChild.moveCh(move3, move3 + d1);
+							maxChild.setValue(maxChild.evaluate(true));
+						
+							for(int move4=0; move4<24; move4++){//TETARTH KINHSH
+								if(board.noMoreMoves(d1)){
+									//MAX
+									for(int i=0; i<21; i++){
+										
+										Board minChild = new Board(Min(maxChild,luck[i][1],luck[i][2],depth + 1, alpha, beta));
+										if(minChild.getValue() > maxMove.getValue() ){  // TRUE EINAI TO MAVRO
+											maxMove.setBoard(minChild.getBoard());
+											maxMove.setValue(minChild.getValue());
+										}
+										if(minChild.getValue() >= beta) return maxMove;
+										if(minChild.getValue() > alpha) alpha = minChild.getValue();
+									}//KLEINEI MAX
+									break;
+								}
+								else if( (move4 + d1 < 24) && board.canMoveCh(move4, move4 + d1)){
+									maxChild.moveCh(move4, move4 + d1);
+
+									for(int i=0; i<21; i++){
+							
+										Board minChild = new Board(Min(maxChild,luck[i][1],luck[i][2],depth + 1, alpha, beta));
+										if(minChild.getValue() > maxMove.getValue() ){  // TRUE EINAI TO MAVRO
+											maxMove.setBoard(minChild.getBoard());
+											maxMove.setValue(minChild.getValue());
+										}
+										if(minChild.getValue() >= beta) return maxMove;
+										if(minChild.getValue() > alpha) alpha = minChild.getValue();
+									}
+			
+								
+
+									
+public Board getNextMove(Board b,int d1,int d2){									
+	if(d1 == d2){//DIPLES
+		if(maxChild.noMoreMoves(d1)){
+			/*
+			//MAX 
+			for(int i=0; i<21; i++){
+				
+				Board minChild = new Board(Min(maxChild,luck[i][1],luck[i][2],depth + 1, alpha, beta));
+				if(minChild.getValue() > maxMove.getValue() ){  // TRUE EINAI TO MAVRO
+					maxMove = minChild;
+				}
+				if(minChild.getValue() >= beta) return maxMove;
+				if(minChild.getValue() > alpha) alpha = minChild.getValue();
+			}//KLEINEI MAX
+			*/
+		}else{
+			for(int i=0; i<24; i++){ 
+				if( (i + d1 < 24) && board.canMoveCh(i, i + d1)){
+					Board maxChild =new Board(b);//to maxChild ginetai to tamplo
+					maxChild.moveCh(i, i + d1);
+					maxChild.setValue(maxChild.evaluate(true));	
+					maxChild.getNextMove(maxChild,int d1,int d2);
+		}
+		
+						
+					
